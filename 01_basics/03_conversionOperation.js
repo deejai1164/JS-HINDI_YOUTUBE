e4c703b7cf0c5ac13c187edcad3a85f94b58a4a1
Deepak Jaiswal <dj628164@gmail.com>
2025-06-16 20:30:52 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,45 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//***************************operations*****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)  // 12
+// console.log(1+"2")  //12
+// console.log("1"+2+2)  //122
+// console.log(1+2+"2") // 32
+
+// console.log(+true);  // 1
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2  // Not readable easily
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter);  // 101
+
+let x = 3;
+const y = x++;
+// console.log(x, y)  // 4 3 
+let a = 3
+const b = ++a
+console.log(a, b)  // 4 4 
