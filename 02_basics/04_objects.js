894ee0799f866e8544b6039d408708d149428d08
Deepak Jaiswal <dj628164@gmail.com>
2025-06-19 17:19:13 +0000

    json obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,7 +17,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName.userfullname.firstname);    // hitesh
+// console.log(regularUser.fullName.userfullname.firstname);    // hitesh
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -26,7 +26,7 @@ const obj2 = {3: "a", 4: "b"}
 // const obj3 = Object.assign({}, obj1, obj2)   // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }    // {} = target   // obj1, obj2 = source
 
 const obj3 = {...obj1, ...obj2}          // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
-console.log(obj3);
+// console.log(obj3);
 
 const users = [
     {
@@ -48,10 +48,45 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));   // [ 'id', 'name', 'isLoggedIn' ]
 // console.log(Object.values(tinderUser));   // [ '123abc', 'Sammy', false ]
-console.log(Object.entries(tinderUser));     // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.entries(tinderUser));     // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLogged'));   false
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));   // false
+
+
+const course = {
+    coursename: "js in hindi",
+    price : "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor: instructor} = course
+// console.log(courseInstructor);    // hitesh
+
+const {courseInstructor: instructor} = course   // destructured from courseInstructor to instructor
+console.log(instructor);
+
+const navbar = ({company}) => {                // Destructuring
+
+}
+
+
+// APIs    // Jab apna kaam kisi aur ke sar par daal dena hota h we use APIs
+
+// {
+//     "name": "hitesh",
+//     "coursename" : "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+    
+]
