dc96b77d619391e0bdfda6a8df25ace754811a66
JS-HINDI_YOUTUBE <dj628164@gmail.com>
2025-06-24 00:26:01 +0530

    project 2

---
+++ 07_02_projects/chaiaurcode.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form')
+
+// this usecase will give you empty 
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+
+    const results = document.querySelector('#results')
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = `please give a valid height ${height}`;
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `please give a valid weight ${weight}`;
+    }else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+        // Determine category
+        let category = '';
+        if (bmi < 18.6) {
+            category = 'Under Weight';
+        } else if (bmi >= 18.6 && bmi <= 24.9) {
+            category = 'Normal Range';
+        } else {
+            category = 'Overweight';
+        }
+
+        // show the results
+        results.innerHTML = `<span>Your BMI is ${bmi} (${category})</span>`;
+    }
+    
+});
